e1eb1cff47a79de7adac6741dd4d6cc6192840c7
blubars <blubars@gmail.com>
2018-03-16 10:58:51 -0600

    parsing progress

--- vis3/parse_data.py
+++ vis3/parse_data.py
@@ -15,8 +15,7 @@ class MaleFemaleVis():
         self.data = {}
         self.header = []
         self.filename = filename
-        self.male = nested_dict()
-        self.female = nested_dict()
+        self.parsed = nested_dict()
 
     """
     Function that opens up the csv file and reads in the data into a large dict
@@ -51,22 +50,20 @@ class MaleFemaleVis():
                 school_year = school_year.split('-')[0]
                 major = self.data[num]['Major Program Name']
                 if not self.is_computer_science(major):
+                    # only look at CS majors for now
                     continue
-                print("[{}, Male]: i:{}, y:{}, m:{}".format(num, institution, school_year, major))
+                print("[{}, M]: i:{}, y:{}, m:{}".format(
+                    num, institution, school_year, major))
                 aggregate = self.get_student_numbers('Male', num)
-                self.male[institution][school_year] = aggregate
-                print(aggregate.keys())
-                for year in aggregate.keys():
-                    total = 0
-                    for race in aggregate[year].keys():
-                        try:
-                            total += int(aggregate[year][race])
-                            print(aggregate[year][race])
-                        except ValueError:
-                            # missing data value!
-                            #continue
-                            pass
-                    print("  year:{}, total:{}".format(year, total))
+                self.parsed[institution][school_year]['Male'] = aggregate
+                print("[{}, F]: i:{}, y:{}, m:{}".format(
+                    num, institution, school_year, major))
+                aggregate = self.get_student_numbers('Female', num)
+                self.parsed[institution][school_year]['Female'] = aggregate
+                #for year in aggregate.keys():
+                #    calc_total = aggregate[year]['calc_tot']
+                #    given_total = aggregate[year]['giv_tot']
+                #    print("  year:{}, calc_total:{}, given_total:{}, \tdiff:{}".format(year, calc_total, given_total, calc_total-given_total))
             except KeyError:
                 #for key in self.data[num].keys():
                 #    print(key)
@@ -75,18 +72,43 @@ class MaleFemaleVis():
                 #print(self.data[num].keys())
                 print("Dammit!")
 
-    def write_data(self, gender):
-        outfile = "filtered-v3-" + gender + ".csv"
-        if gender is "Male":
-            data = self.male
-        else:
-            data = self.female
+    def write_data(self):
+        outfile = "filtered-v3-data.csv"
+        data = self.parsed
         f = open(outfile, 'w', encoding='utf-8')
         writer = csv.writer(f, delimiter=',', quotechar='"')
         for item in data:
             # filter data, write ones that are appropriate
             row = []
             writer.writerow(row)
+
+    def group_by_class(self):
+        grouped_d = nested_dict()
+        for inst in self.parsed.keys():
+            years = [year for year in self.parsed[inst].keys()]
+            years.sort()
+            #print(years)
+            # ignore institutions without >=4 years of data
+            if len(years) >= 4:
+                # is the data within those years valid?
+                for year in years:
+                    valid = True
+                    cls = ['Freshmen', 'Sophomores', 'Juniors', 'Seniors']
+                    class_d = nested_dict()
+                    for i, cl in enumerate(cls):
+                        year_i = str(int(year) + i)
+                        if year_i in years:
+                            d = self.parsed[inst][year_i]['Female']
+                            class_d[i] = d[cl]['calc_tot']
+                        else:
+                            valid = False
+                    # is the class valid for all 4 years? 
+                    # add to dict if so
+                    if valid:
+                        print("{}: {}".format(inst, year))
+                        print(class_d)
+                        grouped_d[inst][year][gen] = class_d
+        self.grouped_dict = grouped_d
 
     def get_student_numbers(self, gender, record_num):
         # extract data from relevant fields
@@ -97,9 +119,9 @@ class MaleFemaleVis():
             #regex = re.compile(pattern)
             #result = prog.match(string)
             if gender in field:
-                if "Enroll" in field:
-                    year = 'Enroll'
-                elif "Freshmen" in field:
+                #if "Enroll" in field:
+                #    year = 'Enroll'
+                if "Freshmen" in field:
                     year = 'Freshmen'
                 elif "Sophomores" in field:
                     year = 'Sophomores'
@@ -107,22 +129,28 @@ class MaleFemaleVis():
                     year = 'Juniors'
                 elif "Seniors" in field:
                     year = 'Seniors'
-                #elif "Totals" in field:
-                #    year = 'Totals'
                 else:
                     continue
                 race = str(field.split(': ')[1].split('(')[0]).strip()
                 if self.valid_race(race):
                     try:
                         value = int(self.data[record_num][field])
-                        d[year][race] = value
-                        if not isinstance(d[year]['Total'], int):
-                            d[year]['Total'] = value
-                        else:
-                            d[year]['Total'] += value
                     except ValueError:
-                        print("Oh noes")
-                        pass
+                        # missing value! data not present. put 0
+                        value = 0
+                    d[year][race] = value
+                    if 'calc_tot' not in d[year]:
+                        d[year]['calc_tot'] = value
+                    else:
+                        d[year]['calc_tot'] += value
+                elif "Total Declared Majors" in field:
+                    # compare total from dataset against calc data
+                    # from adding up races
+                    try:
+                        value = int(self.data[record_num][field])
+                    except ValueError:
+                        value = 0
+                    d[year]['giv_tot'] = value
         return d
 
     def is_computer_science(self, major):
@@ -153,23 +181,9 @@ if __name__ == '__main__':
     vis = MaleFemaleVis()
     vis.read_data()
     vis.format_data()
-    vis.write_data()
+    vis.group_by_class()
+    #vis.write_data()
 
     # year --> major --> race --> #
     #totals = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-
-    # example how to access number of candidates for a school and major over all years
-    # print(vis.female)
-    total = 0
-    for year in vis.female.keys():
-        for major in vis.female[year].keys():
-            for race in vis.female[year][major].keys():
-                print(race)
-
-            import sys
-            sys.exit()
-
-        # total += sum(vis.female[year]['Computer Science'].values_flat())
-
-    #
 
